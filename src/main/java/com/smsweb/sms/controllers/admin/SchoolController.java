a5ff868edd8f4f0f1e92bad3987b3868025a473d
Anoop <anoopmonu02@gmail.com>
2024-07-10 23:36:17 +0530

    Adding school files

--- src/main/java/com/smsweb/sms/controllers/admin/SchoolController.java
+++ src/main/java/com/smsweb/sms/controllers/admin/SchoolController.java
@@ -1,13 +1,17 @@
 package com.smsweb.sms.controllers.admin;
 
 
+import com.smsweb.sms.models.admin.Customer;
 import com.smsweb.sms.models.admin.School;
+import com.smsweb.sms.models.universal.City;
 import com.smsweb.sms.services.admin.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -30,4 +34,18 @@ public class SchoolController {
         model.addAttribute("hasSchoolData", !schools.isEmpty());
         return "/admin/school";
     }
+
+    @GetMapping("/school/add")
+    public String addSchoolForm(Model model){
+        model.addAttribute("school", new School());
+        model.addAttribute("provinces", schoolService.getAllProvinces());
+        return "/admin/add-school";
+    }
+
+    @ResponseBody
+    @GetMapping("/school/cities")
+    public List<City> getCities(@RequestParam Long provinceId) {
+        return schoolService.getAllCitiesByProvince(provinceId);
+    }
+
 }
